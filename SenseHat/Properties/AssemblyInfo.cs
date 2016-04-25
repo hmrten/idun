a920e961471c2aa51a32720890a537f99d90bed8
Mårten Hansson <hmrten@gmail.com>
2016-04-25 08:05:56 +0200

    setup empty SenseHat library project

--- SenseHat/Properties/AssemblyInfo.cs
+++ SenseHat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("idun")]
+[assembly: AssemblyTitle("SenseHat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("idun")]
+[assembly: AssemblyProduct("SenseHat")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
