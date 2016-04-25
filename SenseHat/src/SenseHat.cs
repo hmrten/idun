4990f7eaa9daf2de5c118540113744320674bf3d
Mårten Hansson <hmrten@gmail.com>
2016-04-25 10:28:13 +0200

    refactor SenseHat into a library

---
+++ SenseHat/src/SenseHat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace SenseHat
+{
+    public class SenseHat
+    {
+        private HTS221 hts221;
+        private LPS25H lps25h;
+        private DispatcherTimer timer;
+        private int updateIntervalMS;
+
+        public delegate void SensorReadings(float temperature, float humidity, float pressure);
+
+        public SensorReadings SensorReadingsUpdate { get; set; }
+
+        public SenseHat(int updateIntervalMS)
+        {
+            this.updateIntervalMS = updateIntervalMS;
+        }
+
+        public void Init()
+        {
+            var task = Task.Run(async () =>
+            {
+                hts221 = await Sensor.CreateAsync<HTS221>().ConfigureAwait(false);
+                lps25h = await Sensor.CreateAsync<LPS25H>().ConfigureAwait(false);
+            });
+            if (!task.Wait(5000))
+            {
+                throw new Exception("tiemd out waiting for sensor to initialize");
+            }
+            timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(updateIntervalMS) };
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            var temp = hts221.ReadTemperature();
+            var humid = hts221.ReadHumidity();
+            var press = lps25h.ReadPressure();
+            SensorReadingsUpdate?.Invoke(temp, humid, press);
+        }
+    }
+}
---
+++ SenseHat/src/Sensor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace SenseHat
+{
+    public abstract class Sensor : IDisposable
+    {
+        protected I2cDevice device;
+
+        public virtual void Dispose()
+        {
+            if (device != null)
+            {
+                device.Dispose();
+                device = null;
+            }
+        }
+
+        protected abstract Task InitAsync();
+
+        public static async Task<T> CreateAsync<T>()
+            where T : Sensor, new()
+        {
+            var sensor = new T();
+            await sensor.InitAsync();
+            return sensor;
+        }
+    }
+}
